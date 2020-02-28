866fc37460f9e06a87b324b76f273f3652fffc77
sandeepsreedeep <sandeepsreedeep@gmail.com>
2020-02-29 02:03:44 +0530

    n-th fib new methods added

--- n_th_Fib.py
+++ n_th_Fib.py
@@ -1,3 +1,10 @@
+import time
+
+################################################################################
+#						NAIVE RECURSION
+################################################################################
+
+str_time = time.time()
 def fib(n):
 	if n == 1:
 		return 0
@@ -7,4 +14,44 @@ def fib(n):
 		return fib(n-1)+fib(n-2)
 
 
-print(fib(8))
+print(fib(20))
+print(f'{time.time()-str_time}')
+
+##############################################################################
+#						MEMOIZE RECURSION
+#############################################################################
+
+
+str_time = time.time()
+def mem_fib(n, memoize = {1:0,2:1}):
+	if n in memoize:
+		return memoize[n]
+	else:
+		memoize[n] = mem_fib(n-1,memoize) + mem_fib(n-2,memoize)
+		return memoize[n]
+
+print(mem_fib(20))
+
+print(f'{time.time()-str_time}')
+
+############################################################################
+#						ITERATIVE METHOD
+############################################################################
+
+str_time = time.time()
+def it_fib(n, memoize = {1:0,2:1}):
+	a = 0
+	b = 1
+	sums = 0
+	if n == 1:
+		return a
+	else:
+		for i in range(2,n):
+			sums = a+b
+			a = b
+			b = sums
+	return sums
+
+print(it_fib(20))
+
+print(f'{time.time()-str_time}')
